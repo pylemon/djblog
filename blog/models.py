159a3f1fcada2d6e69004e84153d105148fc4172
pylemon <leeway1985@gmail.com>
2015-05-19 11:17:55 +0800

    add posts models link it to views.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
+# coding=utf8
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    content = models.TextField()
+    author = models.ForeignKey(User)
+    pub_date = models.DateTimeField(db_index=True)
+
+    class Meta:
+        ordering = ('-pub_date',)
+
+    @classmethod
+    def add_post(cls, title, content, author, pub_date):
+        new_post = cls(
+            title=title,
+            content=content,
+            author=author,
+            pub_date=pub_date)
+        new_post.save()
+        return new_post
+
+    @classmethod
+    def get_recent_posts(cls, limit=10):
+        return cls.objects.all()[:limit]
+
+    def get_pub_date_display(self):
+        return self.pub_date.strftime("%Y-%m-%d %H:%m")
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
+# coding=utf8
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from blog.models import Post
 
 
 def home(request):
-    return render_to_response("home.html")
+    posts = Post.get_recent_posts()
+
+    ctx = RequestContext(request)
+    ctx.update({
+        'posts': [{
+            'title': p.title,
+            'content': p.content,
+            'author': p.author,
+            'pub_date': p.get_pub_date_display(),
+        } for p in posts]
+    })
+    return render_to_response("home.html", ctx)
